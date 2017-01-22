ebda08f2801da6d2399aac08b6dc8defa57a3f5d
116dariya <116dariya@gmail.com>
2017-01-22 21:48:36 +0600

    Add files via upload

---
+++ lab1ex4.py
@@ -0,0 +1,13 @@
+a = int(input())
+b = int(input())
+c = int(input())
+if ( a + b ) > c:
+	if ( a + c ) > b:
+		if ( b + c ) > a: 
+			print("YES")
+		else:
+			print("NO")
+	else:
+		print("NO")
+else:
+	print("NO")
